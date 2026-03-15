968e9de9fca73babf3239f18a742f421cac5e390
Alessandro Ruiz Nina <Alessandro.Ruiz0725@jala.university>
2026-03-15 03:09:07 -0400

    refactor: configure .NET 8, EF Core, Swagger, and base data model

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnections")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
